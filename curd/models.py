83c0ae8a4d6dc078694f1964db5143ef5d53a658
brajesh708 <brajeshmewada694@gmail.com>
2024-07-25 21:02:57 +0530

    new modal

--- curd/models.py
+++ curd/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    stu_name=models.CharField(max_length=40)
+    stu_city=models.CharField(max_length=50)
+    stu_email=models.EmailField(max_length=50)
+    def __str__(self):
+        return self.stu_name
+    
